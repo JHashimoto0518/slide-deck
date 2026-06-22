e14a67a95bcb84f431fa94f08ea8873447c361e9
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2026-06-22 22:39:44 +0900

    change: query

--- 20260623-jawsug-tokyo/query/ec2-sg-rule.sql
+++ 20260623-jawsug-tokyo/query/ec2-sg-rule.sql
@@ -4,8 +4,8 @@ select
   sg ->> 'GroupId' as "セキュリティグループID",
   sg ->> 'GroupName' as "セキュリティグループ名",
   sgr.type as "IN/OUT",
-  sgr.ip_protocol as "プロトコル",
-  sgr.from_port as "許可ポート",
+  case when sgr.ip_protocol::text = '-1' then 'すべて' else sgr.ip_protocol::text end as "プロトコル",
+  case when sgr.from_port::text = '-1' then 'すべて' else sgr.from_port::text end as "許可ポート",
   sgr.cidr_ipv4 as "ソース",
   sgr.description as "説明"
 from
