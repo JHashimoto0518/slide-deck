83602ee57811f4f265917ca1a8b8db289175b5c6
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2026-06-22 22:05:23 +0900

    add: ec2 instance to stack

--- 20260623-jawsug-tokyo/iac/cdk-ec2/lib/cdk-ec2-stack.ts
+++ 20260623-jawsug-tokyo/iac/cdk-ec2/lib/cdk-ec2-stack.ts
@@ -27,23 +27,50 @@ export class CdkEc2Stack extends Stack {
     });
 
     // security group
-    const ec2Sg = new ec2.SecurityGroup(this, 'Ec2Sg', {
+    const ec2SgWeb = new ec2.SecurityGroup(this, 'Ec2-Web-Sg', {
       vpc,
       allowAllOutbound: true,
-      description: 'security group for ec2'
+      description: 'security group for ec2 web'
     })
-    ec2Sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anywhere')
+    ec2SgWeb.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anywhere')
+    ec2SgWeb.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'allow https traffic from anywhere')
+    
+    const ec2SgBastion = new ec2.SecurityGroup(this, 'Ec2-Bastion-Sg', {
+      vpc,
+      allowAllOutbound: true,
+      description: 'security group for ec2 bastion'
+    })
+    ec2SgBastion.addIngressRule(ec2.Peer.ipv4('203.0.113.0/24'), ec2.Port.tcp(22), 'allow ssh traffic from example IP address')
 
     // ec2
-    const ec2Ins = new ec2.Instance(this, 'Ec2', {
-      instanceName: 'ec2',
+    const ec2InsWeb = new ec2.Instance(this, 'Ec2-Web', {
+      instanceName: 'ec2-web',
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       machineImage: ec2.MachineImage.latestAmazonLinux2023(),
       vpc,
       vpcSubnets: vpc.selectSubnets({
         subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
       }),
-      securityGroup: ec2Sg,
+      securityGroup: ec2SgWeb,
+      blockDevices: [
+        {
+          deviceName: '/dev/xvda',
+          volume: ec2.BlockDeviceVolume.ebs(8, {
+            encrypted: true
+          }),
+        },
+      ],
+    })
+
+    const ec2InsBastion = new ec2.Instance(this, 'Ec2-Bastion', {
+      instanceName: 'ec2-bastion',
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      vpc,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PUBLIC,
+      }),
+      securityGroup: ec2SgBastion,
       blockDevices: [
         {
           deviceName: '/dev/xvda',
